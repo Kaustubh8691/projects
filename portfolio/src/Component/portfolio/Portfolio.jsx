c5d4cf44eca961f3886651c5c8f7a4da61308a47
Kaustubh8691 <kaustubhnangarepatil8691@gmail.com>
2022-05-12 19:57:46 +0530

    Portfolioo

---
+++ portfolio/src/Component/portfolio/Portfolio.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+// import IMG4 from '../../assets/portfolio4.jpg'
+// import IMG5 from '../../assets/portfolio5.png'
+// import IMG6 from '../../assets/portfolio6.jpg'
+
+
+function Portfolio() {
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: 'created Gym page',
+      github: 'https://github.com/Kaustubh8691',
+      demo: 'https://github.com/Kaustubh8691'
+    },
+    {
+      id: 2,
+      image: IMG2, title: 'chat App', github: 'https://github.com/Kaustubh8691',
+      demo: 'https://github.com/Kaustubh8691'
+    }
+    , {
+      id: 3, image: IMG3, title: 'Portfolio',
+      github: 'https://github.com/Kaustubh8691',
+      demo: 'https://github.com/Kaustubh8691'
+    }
+  ]
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return(
+              <article key={'kaustubh' + id} className='portfolio__iten'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn btn-primary' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Github</a>
+              </div>
+            </article>
+            )
+           
+          })
+        }
+
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio 
